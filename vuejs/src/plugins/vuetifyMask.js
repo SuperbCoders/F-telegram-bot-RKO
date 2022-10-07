a5b080bcac9a90b46a6310a57f47fdbee7df84e0
kin-der-nature <johndoe@example.com>
2022-10-07 15:13:37 +0500

    update(frontend): add package vuetify mask

--- vuejs/src/plugins/vuetifyMask.js
+++ vuejs/src/plugins/vuetifyMask.js
@@ -1,4 +1,5 @@
-import Vue from "vue";  
-import VuetifyMask from "vuetify-mask";  
-Vue.use(VuetifyMask);  
+import Vue from "vue";
+import VuetifyMask from "vuetify-mask";
+
+Vue.use(VuetifyMask);
 export default VuetifyMask;
