92561ef127cdd2cab4eba45460d3db362a21d12b
kin-der-nature <johndoe@example.com>
2022-10-07 15:12:55 +0500

    update(frontend): default input

---
+++ vuejs/src/components/input/defaltInput.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="form_block mt-5">
+        <p class="text-left">Адрес</p>
+        <v-text-field
+            id="oldName"
+            placeholder="ООО Ромашка"
+            class="align-center border-none"
+            name="oldName"
+            outlined
+            :required="true"
+        ></v-text-field>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        label: String,
+        placeholder: String
+    }
+}
+</script>
+
+<style>
+
+</style>
