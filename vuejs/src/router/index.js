98fa68380f060ed53638d85665c5dbf78034917e
kin-der-nature <johndoe@example.com>
2022-10-11 17:42:43 +0500

    frontend(add): add path

--- vuejs/src/router/index.js
+++ vuejs/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import LoanForm from "../views/loan/LoanForm";
 import AuthView from '../views/Auth/AuthView.vue'
+import Address from '../views/Address/AdressView.vue'
 Vue.use(VueRouter);
 
 const routes = [
@@ -15,6 +16,11 @@ const routes = [
     name: "auth-form",
     component: AuthView
   },
+  {
+    path: "/address",
+    name: "address-form",
+    component: Address
+  }
 ];
 
 const router = new VueRouter({
