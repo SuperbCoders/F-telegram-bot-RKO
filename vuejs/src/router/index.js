f3399dc0b992ea2e37046e9911ec665b3ca02be2
kin-der-nature <johndoe@example.com>
2022-10-13 12:44:46 +0500

    add(frontend): routing pages

--- vuejs/src/router/index.js
+++ vuejs/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import LoanForm from "../views/loan/LoanForm";
+// import LoanForm from "../views/loan/LoanForm";
 import AuthView from '../views/Auth/AuthView.vue'
 import Address from '../views/Address/AdressView.vue'
 import Structure from '../views/Structure/StructureForm.vue'
@@ -13,13 +13,13 @@ import Rate from '../views/Rate/RateForm.vue'
 Vue.use(VueRouter);
 
 const routes = [
+  // {
+  //   path: "/",
+  //   name: "loan-form",
+  //   component: LoanForm,
+  // },
   {
     path: "/",
-    name: "loan-form",
-    component: LoanForm,
-  },
-  {
-    path: "/auth",
     name: "auth-form",
     component: AuthView
   },
