6fbffaf1d7d97daf6f651462429eb20be16b2b8d
kin_der_nature <kin_der_nature@mail.ru>
2022-10-14 04:32:46 +0500

    update(frontend): new pages path

--- vuejs/src/router/index.js
+++ vuejs/src/router/index.js
@@ -10,6 +10,13 @@ import informationStaff from '../views/informationStaff/informationStaffForm.vue
 import CreditForm from '../views/credit/creditForm.vue'
 import DocumentPage from '../views/Documents/documentForm.vue'
 import individualsForm from '../views/informationIndivduals/individualsForm.vue'
+import DocumentsForms from '../views/DocumentsForm/DocumentForm.vue'
+import AddressForm from '../views/AddressForm/AddressForm.vue'
+import KinshipStatus from '../views/KinshipStatus/KinshipStatusForm.vue'
+import ForeignPerson from '../views/isAForeignPerson/ForeignPerson.vue'
+import EmailForm from '../views/EmailForm/EmailForm.vue'
+import ClientInfo from '../views/ClientInfo/ClientInfoForm.vue'
+import DocumentFogeiner from '../views/DocumentFogeiner/DocumentFogeinerForm.vue'
 import Rate from '../views/Rate/RateForm.vue'
 Vue.use(VueRouter);
 
@@ -50,6 +57,16 @@ const routes = [
     component: Intelligence
   },
   {
+    path: "/email-form",
+    name: "email-form",
+    component: EmailForm
+  },
+  {
+    path: "/client-info",
+    name: "client-info",
+    component: ClientInfo
+  },
+  {
     path: "/credit-page",
     name: "credit-page",
     component: CreditForm
@@ -63,6 +80,31 @@ const routes = [
     path: "/individual-info",
     name: "individual-info",
     component: individualsForm
+  },
+  {
+    path: "/documents-forms",
+    name: "documents-forms",
+    component: DocumentsForms
+  },
+  {
+    path: "/kinship-status-forms",
+    name: "kinship-status-forms",
+    component: KinshipStatus
+  },
+  {
+    path: "/address-form",
+    name: "address-form2",
+    component: AddressForm
+  },
+  {
+    path: "/document-fogeiner",
+    name: "document-fogeiner",
+    component: DocumentFogeiner
+  },
+  {
+    path: "/foreign-person",
+    name: "foreign-person",
+    component: ForeignPerson
   },
   {
     path: "/rate",
