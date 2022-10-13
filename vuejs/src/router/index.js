db35fd1ca21a12c3f5dd077d04da0b3d9aae9399
kin-der-nature <johndoe@example.com>
2022-10-13 14:13:10 +0500

    udpate(frontend): return loan form path

--- vuejs/src/router/index.js
+++ vuejs/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-// import LoanForm from "../views/loan/LoanForm";
+import LoanForm from "../views/loan/LoanForm";
 import AuthView from '../views/Auth/AuthView.vue'
 import Address from '../views/Address/AdressView.vue'
 import Structure from '../views/Structure/StructureForm.vue'
@@ -13,11 +13,11 @@ import Rate from '../views/Rate/RateForm.vue'
 Vue.use(VueRouter);
 
 const routes = [
-  // {
-  //   path: "/",
-  //   name: "loan-form",
-  //   component: LoanForm,
-  // },
+  {
+    path: "/loan-form",
+    name: "loan-form",
+    component: LoanForm,
+  },
   {
     path: "/",
     name: "auth-form",
