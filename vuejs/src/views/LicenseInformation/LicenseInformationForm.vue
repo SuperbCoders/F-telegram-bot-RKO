032de679da04ae3a26f187597c39a123cee2fcb1
kin-der-nature <johndoe@example.com>
2022-10-14 10:30:04 +0500

    update(frontend): redirect step

--- vuejs/src/views/LicenseInformation/LicenseInformationForm.vue
+++ vuejs/src/views/LicenseInformation/LicenseInformationForm.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="license_information_form">
-    <h2 class="form_block_label">
-        Сведения о лицензии
-    </h2>
+    <h2 class="form_block_label">Сведения о лицензии</h2>
     <div class="form_block mt-5">
       <p class="text-left form_block_title">Вид</p>
       <v-text-field
@@ -39,29 +37,96 @@
         :required="true"
       ></v-text-field>
     </div>
-    <div class="form_block mt-5">
-      <p class="text-left form_block_title">Численность персонала</p>
-      <v-text-field
-        id="oldName"
-        placeholder="Наименование"
-        class="align-center border-none"
-        name="oldName"
-        :rules="requiredRules"
-        outlined
-        :required="true"
-      ></v-text-field>
+    <div class="form_block">
+      <p class="text-left form_block_title">
+        Дата выдачи лицензии
+      </p>
+      <v-menu
+        :close-on-content-click="false"
+        transition="scale-transition"
+        offset-y
+        min-width="auto"
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-text-field
+            placeholder="Дата"
+            id="passportIssueDate"
+            name="passportIssueDate"
+            outlined
+            append-icon="mdi-calendar-blank"
+            readonly
+            v-model="dateStarting"
+            :rules="requiredRules"
+            :required="true"
+            v-bind="attrs"
+            v-on="on"
+          ></v-text-field>
+        </template>
+        <v-date-picker
+          v-model="dateStarting"
+          @input="passportIssueDateMenu = false"
+        ></v-date-picker>
+      </v-menu>
+    </div>
+    <div class="form_block">
+      <p class="text-left form_block_title">
+        Срок дей
+      </p>
+      <v-menu
+        :close-on-content-click="false"
+        transition="scale-transition"
+        offset-y
+        min-width="auto"
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-text-field
+            placeholder="Дата"
+            id="passportIssueDate"
+            name="passportIssueDate"
+            outlined
+            append-icon="mdi-calendar-blank"
+            readonly
+            v-model="dateStarting"
+            :rules="requiredRules"
+            :required="true"
+            v-bind="attrs"
+            v-on="on"
+          ></v-text-field>
+        </template>
+        <v-date-picker
+          v-model="dateStarting"
+          @input="passportIssueDateMenu = false"
+        ></v-date-picker>
+      </v-menu>
     </div>
     <div class="form_block mt-5">
-      <p class="text-left form_block_title">Численность персонала</p>
-      <v-text-field
-        id="oldName"
-        placeholder="Наименование"
-        class="align-center border-none"
-        name="oldName"
-        :rules="requiredRules"
-        outlined
-        :required="true"
-      ></v-text-field>
+      <p class="text-left form_block_title">Срок действия</p>
+      <v-menu
+        :close-on-content-click="false"
+        transition="scale-transition"
+        offset-y
+        min-width="auto"
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-text-field
+            placeholder="Дата"
+            id="passportIssueDate"
+            name="passportIssueDate"
+            outlined
+            append-icon="mdi-calendar-blank"
+            readonly
+            v-model="dateStarting"
+            :rules="requiredRules"
+            :required="true"
+            v-bind="attrs"
+            v-on="on"
+          ></v-text-field>
+        </template>
+        <v-date-picker
+          v-model="dateStarting"
+          @input="passportIssueDateMenu = false"
+        ></v-date-picker>
+      </v-menu>
     </div>
     <div class="form_block mt-5">
       <p class="text-left form_block_title">Численность персонала</p>
@@ -79,11 +144,8 @@
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style>
