20ee295b1283b6202df420314f68dc53a0f00231
kin-der-nature <johndoe@example.com>
2022-10-14 08:22:14 +0500

    update(frontend): validation

--- vuejs/src/views/Structure/StructureForm.vue
+++ vuejs/src/views/Structure/StructureForm.vue
@@ -26,7 +26,7 @@
           class="align-center border-none"
           name="oldName"
           outlined
-          :rules="requiredRules"
+          :rules="innRules"
         ></v-text-field>
       </div>
       <div class="form_group">
@@ -103,6 +103,13 @@ export default {
     valid: true,
     isTest1: false,
     isTest2: false,
+    innRules: [
+      (v) => !!v || "Это поле обязательно",
+      (v) =>
+        (v && v.length >= 10) || "ИНН не может содержать меньше 10 симоволов",
+      (v) =>
+        (v && v.length <= 12) || "ИНН не может содержать больше 12 симоволов",
+    ],
     requiredRules: [(v) => !!v || "Это поле обязательно"]
   }),
 
