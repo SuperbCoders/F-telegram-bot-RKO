6cf3893dc3e2ac5a07f582454e5400a0198c884e
kin-der-nature <johndoe@example.com>
2022-10-14 11:20:44 +0500

    update(frontend): redirect step

--- vuejs/src/views/Documents/documentForm.vue
+++ vuejs/src/views/Documents/documentForm.vue
@@ -1,25 +1,38 @@
 <template>
   <div class="document_section">
-    <h2 class="text-left mb-10 ">Документы</h2>
+    <h2 class="text-left mb-10">Документы</h2>
     <p class="subtitle text-left document_section_text mb-5">
       Сведения о лицензии на право осуществления деятельности
     </p>
-    <AttachButton class="mb-5 " text="Загрузить документ" />
-    <v-btn block large class="mt-10 auth_form_bth" color="primary">
-      <router-link class="auth_form_bth color-white text-decoration-none" to="/client-info"> Продолжить </router-link>
+    <AttachButton class="mb-5" text="Загрузить документ" />
+    <v-btn
+      block
+      large
+      :disabled="!valid"
+      class="mt-10 auth_form_bth"
+      color="primary"
+      @click="redirect()"
+      >Продолжить
     </v-btn>
   </div>
 </template>
 
 <script>
-import AttachButton from '../../components/button/attachButton.vue';
+import AttachButton from "../../components/button/attachButton.vue";
 
-export default { components: { AttachButton } };
+export default {
+  components: { AttachButton },
+  methods: {
+    redirect () {
+      this.$router.push('/client-info')
+    }
+  }
+  };
 </script>
 
 <style scoped>
 .document_section_text {
-    font-family: Roboto;
-    font-size: 18px;
+  font-family: Roboto;
+  font-size: 18px;
 }
 </style>
