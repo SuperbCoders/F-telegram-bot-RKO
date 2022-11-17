dbce599287990e2dc3c82dd66ad92393c5ec25df
l4legenda <l4legenda@gmail.com>
2022-11-17 17:43:57 +0500

    feat: split

--- django/project/settings/base.py
+++ django/project/settings/base.py
@@ -8,7 +8,7 @@ BASE_DIR = os.path.dirname(PROJECT_DIR)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['django', 'localhost', '127.0.0.1', os.getenv('ALLOWED_DOMAIN_NAME')]
+ALLOWED_HOSTS = os.getenv('ALLOWED_DOMAIN_NAME').split(',')
 
 
 SECRET_KEY = os.getenv('SECRET_KEY')
