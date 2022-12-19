40d9aca556fb35c72fa9780173d10241b9cdd977
l4legenda <l4legenda@gmail.com>
2022-12-19 18:19:55 +0500

    update: approvals and purposes

--- vuejs/src/views/step_8/Purposes/PurposesForm.vue
+++ vuejs/src/views/step_8/Purposes/PurposesForm.vue
@@ -3,6 +3,339 @@
     <h2>Сведения о бизнесе</h2>
     <div>
       <h4 class="form_block_label text-left">
+        Компания осуществляет деятельность, подлежащую лицензированию
+      </h4>
+      <div @click="valid = true" class="form_block">
+        <v-radio-group v-model="currentData.subject_licensing" column>
+          <v-radio label="Осуществляет" value="Осуществляет"></v-radio>
+          <v-radio label="Не осуществляет" value="Не осуществляет"></v-radio>
+        </v-radio-group>
+      </div>
+      <p v-if="!valid && !isValidateInformationGoals" class="error_message">Выберите поле</p>
+    </div>
+    <div>
+      <h4 class="form_block_label text-left">
+        История, репутация, сектор рынка и конкуренция
+      </h4>
+      <div @click="valid = true" class="form_block">
+        <v-radio-group v-model="currentData.history_reputation" column>
+          <v-radio label="Положительная" value="Положительная"></v-radio>
+          <v-radio label="Отрицательная" value="Отрицательная"></v-radio>
+        </v-radio-group>
+      </div>
+      <p v-if="!valid && !isValidateInformationGoals" class="error_message">Выберите поле</p>
+    </div>
+
+    <div>
+      <h4 class="form_block_label text-left">
+        Общее количество операций в месяц
+      </h4>
+      <div @click="valid = true" class="form_block">
+        <v-radio-group v-model="currentData.num_transactions_month" column>
+          <v-radio label="От 10" value="От 10"></v-radio>
+          <v-radio label="От 100" value="От 100"></v-radio>
+          <v-radio label="От 1000" value="От 1000"></v-radio>
+        </v-radio-group>
+      </div>
+      <p v-if="!valid && !isValidateInformationGoals" class="error_message">Выберите поле</p>
+    </div>
+
+    <div>
+      <h4 class="form_block_label text-left">
+        Общее количество операций в неделю
+      </h4>
+      <div @click="valid = true" class="form_block">
+        <v-radio-group v-model="currentData.num_transactions_week" column>
+          <v-radio label="От 10" value="От 10"></v-radio>
+          <v-radio label="От 100" value="От 100"></v-radio>
+          <v-radio label="От 1000" value="От 1000"></v-radio>
+        </v-radio-group>
+      </div>
+      <p v-if="!valid && !isValidateInformationGoals" class="error_message">Выберите поле</p>
+    </div>
+
+    <div>
+      <h4 class="form_block_label text-left">
+        Общее количество операций в квартал
+      </h4>
+      <div @click="valid = true" class="form_block">
+        <v-radio-group v-model="currentData.num_transactions_quarter" column>
+          <v-radio label="От 10" value="От 10"></v-radio>
+          <v-radio label="От 100" value="От 100"></v-radio>
+          <v-radio label="От 1000" value="От 1000"></v-radio>
+        </v-radio-group>
+      </div>
+      <p v-if="!valid && !isValidateInformationGoals" class="error_message">Выберите поле</p>
+    </div>
+
+    <div>
+      <h4 class="form_block_label text-left">
+        Общее количество операций в год
+      </h4>
+      <div @click="valid = true" class="form_block">
+        <v-radio-group v-model="currentData.num_transactions_age" column>
+          <v-radio label="От 10" value="От 10"></v-radio>
+          <v-radio label="От 100" value="От 100"></v-radio>
+          <v-radio label="От 1000" value="От 1000"></v-radio>
+        </v-radio-group>
+      </div>
+      <p v-if="!valid && !isValidateInformationGoals" class="error_message">Выберите поле</p>
+    </div>
+
+    <div>
+      <h4 class="form_block_label text-left">
+        Общая сумма операций в месяц
+      </h4>
+      <div @click="valid = true" class="form_block">
+        <v-radio-group v-model="currentData.sum_transactions_month" column>
+          <v-radio label="До 1 000 000" value="До 1 000 000"></v-radio>
+          <v-radio label="До 10 000 000" value="До 10 000 000"></v-radio>
+          <v-radio label="До 100 000 000" value="До 100 000 000"></v-radio>
+          <v-radio label="Свыше 100 000 000" value="Свыше 100 000 000"></v-radio>
+        </v-radio-group>
+      </div>
+      <p v-if="!valid && !isValidateInformationGoals" class="error_message">Выберите поле</p>
+    </div>
+
+    <div>
+      <h4 class="form_block_label text-left">
+        Общая сумма операций в квартал
+      </h4>
+      <div @click="valid = true" class="form_block">
+        <v-radio-group v-model="currentData.sum_transactions_quarter" column>
+          <v-radio label="До 1 000 000" value="До 1 000 000"></v-radio>
+          <v-radio label="До 10 000 000" value="До 10 000 000"></v-radio>
+          <v-radio label="До 100 000 000" value="До 100 000 000"></v-radio>
+          <v-radio label="Свыше 100 000 000" value="Свыше 100 000 000"></v-radio>
+        </v-radio-group>
+      </div>
+      <p v-if="!valid && !isValidateInformationGoals" class="error_message">Выберите поле</p>
+    </div>
+
+    <div>
+      <h4 class="form_block_label text-left">
+        Общая сумма операций в год
+      </h4>
+      <div @click="valid = true" class="form_block">
+        <v-radio-group v-model="currentData.sum_transactions_age" column>
+          <v-radio label="До 1 000 000" value="До 1 000 000"></v-radio>
+          <v-radio label="До 10 000 000" value="До 10 000 000"></v-radio>
+          <v-radio label="До 100 000 000" value="До 100 000 000"></v-radio>
+          <v-radio label="Свыше 100 000 000" value="Свыше 100 000 000"></v-radio>
+        </v-radio-group>
+      </div>
+      <p v-if="!valid && !isValidateInformationGoals" class="error_message">Выберите поле</p>
+    </div>
+
+    <div>
+      <h4 class="form_block_label text-left">
+        Количество операций по снятию наличности в месяц
+      </h4>
+      <div @click="valid = true" class="form_block">
+        <v-radio-group v-model="currentData.monthly_cash_withdrawal" column>
+          <v-radio label="От 10" value="От 10"></v-radio>
+          <v-radio label="От 100" value="От 100"></v-radio>
+          <v-radio label="От 1000" value="От 1000"></v-radio>
+        </v-radio-group>
+      </div>
+      <p v-if="!valid && !isValidateInformationGoals" class="error_message">Выберите поле</p>
+    </div>
+
+    <div>
+      <h4 class="form_block_label text-left">
+        Сумма операций по снятию денежных средств в наличной форме в неделю
+      </h4>
+      <div @click="valid = true" class="form_block">
+        <v-radio-group v-model="currentData.week_cash_withdrawal" column>
+          <v-radio label="От 10" value="От 10"></v-radio>
+          <v-radio label="От 100" value="От 100"></v-radio>
+          <v-radio label="От 1000" value="От 1000"></v-radio>
+        </v-radio-group>
+      </div>
+      <p v-if="!valid && !isValidateInformationGoals" class="error_message">Выберите поле</p>
+    </div>
+
+    <div>
+      <h4 class="form_block_label text-left">
+        Сумма операций по снятию денежных средств в наличной форме в квартал
+      </h4>
+      <div @click="valid = true" class="form_block">
+        <v-radio-group v-model="currentData.quarter_cash_withdrawal" column>
+          <v-radio label="От 10" value="От 10"></v-radio>
+          <v-radio label="От 100" value="От 100"></v-radio>
+          <v-radio label="От 1000" value="От 1000"></v-radio>
+        </v-radio-group>
+      </div>
+      <p v-if="!valid && !isValidateInformationGoals" class="error_message">Выберите поле</p>
+    </div>
+
+    <div>
+      <h4 class="form_block_label text-left">
+        Сумма операций по снятию денежных средств в наличной форме в год
+      </h4>
+      <div @click="valid = true" class="form_block">
+        <v-radio-group v-model="currentData.age_cash_withdrawal" column>
+          <v-radio label="От 10" value="От 10"></v-radio>
+          <v-radio label="От 100" value="От 100"></v-radio>
+          <v-radio label="От 1000" value="От 1000"></v-radio>
+        </v-radio-group>
+      </div>
+      <p v-if="!valid && !isValidateInformationGoals" class="error_message">Выберите поле</p>
+    </div>
+
+    <div>
+      <h4 class="form_block_label text-left">
+        Количество операций по внешнеторговым контрактам в месяц
+      </h4>
+      <div @click="valid = true" class="form_block">
+        <v-radio-group v-model="currentData.foreign_trade_contracts_month" column>
+          <v-radio label="От 10" value="От 10"></v-radio>
+          <v-radio label="От 100" value="От 100"></v-radio>
+          <v-radio label="От 1000" value="От 1000"></v-radio>
+        </v-radio-group>
+      </div>
+      <p v-if="!valid && !isValidateInformationGoals" class="error_message">Выберите поле</p>
+    </div>
+
+    <div>
+      <h4 class="form_block_label text-left">
+        Количество операций по внешнеторговым контрактам в неделю
+      </h4>
+      <div @click="valid = true" class="form_block">
+        <v-radio-group v-model="currentData.foreign_trade_contracts_week" column>
+          <v-radio label="От 10" value="От 10"></v-radio>
+          <v-radio label="От 100" value="От 100"></v-radio>
+          <v-radio label="От 1000" value="От 1000"></v-radio>
+        </v-radio-group>
+      </div>
+      <p v-if="!valid && !isValidateInformationGoals" class="error_message">Выберите поле</p>
+    </div>
+
+    <div>
+      <h4 class="form_block_label text-left">
+        Количество операций по внешнеторговым контрактам в квартал
+      </h4>
+      <div @click="valid = true" class="form_block">
+        <v-radio-group v-model="currentData.foreign_trade_contracts_quarter" column>
+          <v-radio label="От 10" value="От 10"></v-radio>
+          <v-radio label="От 100" value="От 100"></v-radio>
+          <v-radio label="От 1000" value="От 1000"></v-radio>
+        </v-radio-group>
+      </div>
+      <p v-if="!valid && !isValidateInformationGoals" class="error_message">Выберите поле</p>
+    </div>
+
+    <div>
+      <h4 class="form_block_label text-left">
+        Количество операций по внешнеторговым контрактам в год
+      </h4>
+      <div @click="valid = true" class="form_block">
+        <v-radio-group v-model="currentData.foreign_trade_contracts_age" column>
+          <v-radio label="От 10" value="От 10"></v-radio>
+          <v-radio label="От 100" value="От 100"></v-radio>
+          <v-radio label="От 1000" value="От 1000"></v-radio>
+        </v-radio-group>
+      </div>
+      <p v-if="!valid && !isValidateInformationGoals" class="error_message">Выберите поле</p>
+    </div>
+
+    <div>
+      <h4 class="form_block_label text-left">
+        Сумма операций по внешнеторговым контрактам в месяц
+      </h4>
+      <div @click="valid = true" class="form_block">
+        <v-radio-group v-model="currentData.foreign_sum_contracts_month" column>
+          <v-radio label="До 1 000 000" value="До 1 000 000"></v-radio>
+          <v-radio label="До 10 000 000" value="До 10 000 000"></v-radio>
+          <v-radio label="До 100 000 000" value="До 100 000 000"></v-radio>
+          <v-radio label="Свыше 100 000 000" value="Свыше 100 000 000"></v-radio>
+        </v-radio-group>
+      </div>
+      <p v-if="!valid && !isValidateInformationGoals" class="error_message">Выберите поле</p>
+    </div>
+
+    <div>
+      <h4 class="form_block_label text-left">
+        Сумма операций по внешнеторговым контрактам в неделю
+      </h4>
+      <div @click="valid = true" class="form_block">
+        <v-radio-group v-model="currentData.foreign_sum_contracts_week" column>
+          <v-radio label="До 1 000 000" value="До 1 000 000"></v-radio>
+          <v-radio label="До 10 000 000" value="До 10 000 000"></v-radio>
+          <v-radio label="До 100 000 000" value="До 100 000 000"></v-radio>
+          <v-radio label="Свыше 100 000 000" value="Свыше 100 000 000"></v-radio>
+        </v-radio-group>
+      </div>
+      <p v-if="!valid && !isValidateInformationGoals" class="error_message">Выберите поле</p>
+    </div>
+
+    <div>
+      <h4 class="form_block_label text-left">
+        Сумма операций по внешнеторговым контрактам в квартал
+      </h4>
+      <div @click="valid = true" class="form_block">
+        <v-radio-group v-model="currentData.foreign_sum_contracts_quarter" column>
+          <v-radio label="От 10" value="От 10"></v-radio>
+          <v-radio label="От 100" value="От 100"></v-radio>
+          <v-radio label="От 1000" value="От 1000"></v-radio>
+        </v-radio-group>
+      </div>
+      <p v-if="!valid && !isValidateInformationGoals" class="error_message">Выберите поле</p>
+    </div>
+
+    <div>
+      <h4 class="form_block_label text-left">
+        Сумма операций по внешнеторговым контрактам в год
+      </h4>
+      <div @click="valid = true" class="form_block">
+        <v-radio-group v-model="currentData.foreign_sum_contracts_age" column>
+          <v-radio label="От 10" value="От 10"></v-radio>
+          <v-radio label="От 100" value="От 100"></v-radio>
+          <v-radio label="От 1000" value="От 1000"></v-radio>
+        </v-radio-group>
+      </div>
+      <p v-if="!valid && !isValidateInformationGoals" class="error_message">Выберите поле</p>
+    </div>
+
+    <div>
+      <h4 class="form_block_label text-left">
+        Источники поступления денежных средств
+      </h4>
+      <div @click="valid = true" class="form_block">
+        <v-checkbox label="Финансирование учредителей/участников" value="Финансирование учредителей/участников"
+          v-model="currentData.sources_cash_receipts" hide-details></v-checkbox>
+        <v-checkbox label="Доходы от основного вида деятельности" value="Доходы от основного вида деятельности"
+          v-model="currentData.sources_cash_receipts" hide-details></v-checkbox>
+        <v-checkbox label="Доходы от дополнительных видов деятельности"
+          value="Доходы от дополнительных видов деятельности" v-model="currentData.sources_cash_receipts"
+          hide-details></v-checkbox>
+        <v-checkbox label="Заемные (кредитные)/привлеченные денежные средства"
+          value="Заемные (кредитные)/привлеченные денежные средства" v-model="currentData.sources_cash_receipts"
+          hide-details></v-checkbox>
+        <v-checkbox label="Государственное финансирование" value="Государственное финансирование"
+          v-model="currentData.sources_cash_receipts" hide-details></v-checkbox>
+        <v-checkbox label="Иное" value="Иное" v-model="currentData.sources_cash_receipts" hide-details></v-checkbox>
+      </div>
+      <p v-if="!valid && !isValidateInformationGoals" class="error_message">Выберите поле</p>
+    </div>
+
+    <div>
+      <h4 class="form_block_label text-left">
+        Штатная численность сотрудников
+      </h4>
+      <div @click="valid = true" class="form_block">
+        <v-radio-group v-model="currentData.headcount" column>
+          <v-radio label="Единственным работником является единоличный исполнительный орган" value="Единственным работником является единоличный исполнительный орган"></v-radio>
+          <v-radio label="2-5" value="2-5"></v-radio>
+          <v-radio label="6-20" value="6-20"></v-radio>
+          <v-radio label="Более 20" value="Более 20"></v-radio>
+        </v-radio-group>
+      </div>
+      <p v-if="!valid && !isValidateInformationGoals" class="error_message">Выберите поле</p>
+    </div>
+
+    <div>
+      <h4 class="form_block_label text-left">
         Сведения о целях установления деловых отношений с банком
       </h4>
       <div @click="valid = true" class="form_block">
