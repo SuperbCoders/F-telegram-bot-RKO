24e74dd354080da0201933987c6e93246cb4a111
l4legenda <l4legenda@gmail.com>
2022-12-26 15:42:15 +0500

    delete vue-closable

--- vuejs/src/main.js
+++ vuejs/src/main.js
@@ -4,10 +4,8 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
 import VueTheMask from 'vue-the-mask';
-import VueClosable from 'vue-closable'
 
 Vue.use(VueTheMask);
-Vue.use(VueClosable);
 
 Vue.config.productionTip = false
 
